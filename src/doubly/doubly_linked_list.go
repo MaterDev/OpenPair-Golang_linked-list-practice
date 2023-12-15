0abb39a800d3d981baf42c2809b199674e3ee926
Key Clark <aryeh.the.lion@gmail.com>
2023-12-15 01:54:28 -0600

    update doubly linked list with todo outline in comments

--- src/doubly/doubly_linked_list.go
+++ src/doubly/doubly_linked_list.go
@@ -2,6 +2,19 @@ package doubly
 
 import "fmt"
 
-func DoublyLinkedList() {
+//TODO: Create Node struct
+
+//TODO: Create DoublyLinkedList struct
+	// fields: head, tail
+
+//TODO: Create AddToStart(data int) method for DoublyLinkedList
+
+//TODO: Create AddToEnd(data int) method for DoublyLinkedList
+
+//TODO: Create Diplay() method for DoublyLinkedList
+
+//TODO: Create DisplayReverse() method for DoublyLinkedList
+
+func LinkedList() {
 	fmt.Println("Doubly Linked List!")
 }
--- src/main.go
+++ src/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	// "github.com/materdev/linkedlist/src/basic"
-	"github.com/materdev/linkedlist/src/advanced"
+	// "github.com/materdev/linkedlist/src/advanced"
+	"github.com/materdev/linkedlist/src/doubly"
 )
 
 func main() {
 	// basic.BasicLinkedList()
-	advanced.AdvancedLinkedList()
-	// DoublyLinkedList()
+	// advanced.AdvancedLinkedList()
+	doubly.LinkedList()
 }
